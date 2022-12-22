8107008395a8279e9d30e2d0137b3026bf19f5bf
ajay <ap221882@gmail.com>
2022-12-22 18:47:26 +0530

    feat: global styles

--- 3-official-next-app/nextjs-blog/pages/index.js
+++ 3-official-next-app/nextjs-blog/pages/index.js
@@ -56,8 +56,7 @@ export default function Home() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Powered by{" "}
-          <img src='/vercel.svg' alt='Vercel' className={styles.logo} />
+          Powered by Vercel
         </a>
       </footer>
 
